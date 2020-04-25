a1226ab96cc43f2496db9f28263345d44d516163
PatrickDeThin <zeltkimo@gmail.com>
2020-04-25 14:57:45 +0800

    fin 4 real

--- app/src/main/java/cool/very/brad38/MainActivity.java
+++ app/src/main/java/cool/very/brad38/MainActivity.java
@@ -13,24 +13,48 @@ import android.util.Log;
 import android.view.View;
 
 import com.inuker.bluetooth.library.BluetoothClient;
+import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
 import com.inuker.bluetooth.library.connect.listener.BluetoothStateListener;
+import com.inuker.bluetooth.library.connect.options.BleConnectOptions;
 import com.inuker.bluetooth.library.connect.response.BleConnectResponse;
+import com.inuker.bluetooth.library.connect.response.BleNotifyResponse;
+import com.inuker.bluetooth.library.model.BleGattCharacter;
 import com.inuker.bluetooth.library.model.BleGattProfile;
+import com.inuker.bluetooth.library.model.BleGattService;
 import com.inuker.bluetooth.library.search.SearchRequest;
 import com.inuker.bluetooth.library.search.SearchResult;
 import com.inuker.bluetooth.library.search.response.SearchResponse;
 
+import java.util.List;
+import java.util.UUID;
+
 import static com.inuker.bluetooth.library.Constants.REQUEST_SUCCESS;
+import static com.inuker.bluetooth.library.Constants.STATUS_CONNECTED;
+import static com.inuker.bluetooth.library.Constants.STATUS_DISCONNECTED;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothClient mClient;
     private final BluetoothStateListener mBluetoothStateListener = new BluetoothStateListener() {
         @Override
         public void onBluetoothStateChanged(boolean openOrClosed) {
-            Log.v("brad",openOrClosed?"OPEN":"CLOSE");
+            Log.v("brad", openOrClosed?"Open":"Close");
         }
 
     };
+
+    private final BleConnectStatusListener mBleConnectStatusListener = new BleConnectStatusListener() {
+
+        @Override
+        public void onConnectStatusChanged(String mac, int status) {
+            if (status == STATUS_CONNECTED) {
+                Log.v("brad", "connected");
+                MainActivity.this.notify(null);
+            } else if (status == STATUS_DISCONNECTED) {
+                Log.v("brad", "disconnected");
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +64,10 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // No explanation needed; request the permission
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     123);
-
-        } else {
+        }else{
             init();
         }
     }
@@ -59,31 +81,51 @@ public class MainActivity extends AppCompatActivity {
     private void init(){
         mClient = new BluetoothClient(this);
         mClient.registerBluetoothStateListener(mBluetoothStateListener);
-        mClient.openBluetooth();                //開藍芽   此處也可再加上mClient.isBluetootheOpened 和一個boolean判斷原本狀態,若開則不用幫他關
+        mClient.openBluetooth();
     }
 
-    // start scan
     public void at1(View view) {
         SearchRequest request = new SearchRequest.Builder()
-                .searchBluetoothLeDevice(3000, 3)   // 先掃BLE設備3次，每次3s
-                .searchBluetoothClassicDevice(5000) // 再掃經典藍芽5s
-                .searchBluetoothLeDevice(2000)      // 再掃BLE設備2s
+                .searchBluetoothLeDevice(3000, 3)   // 先扫BLE设备3次，每次3s
+                .searchBluetoothClassicDevice(5000) // 再扫经典蓝牙5s
+                .searchBluetoothLeDevice(2000)      // 再扫BLE设备2s
                 .build();
-        mClient.search(request, new MySearchListener());
+
+        mClient.search(request, new MySearchListener() );
     }
 
-    // stop scan
     public void at2(View view) {
         mClient.stopSearch();
     }
 
     public void at3(View view) {
-        Log.v("brad","connecting...");
-        mClient.connect(connectMAC, new BleConnectResponse() {
+        Log.v("brad", "connecting...");
+
+        BleConnectOptions options = new BleConnectOptions.Builder()
+                .setConnectRetry(3)   // 连接如果失败重试3次
+                .setConnectTimeout(30000)   // 连接超时30s
+                .setServiceDiscoverRetry(3)  // 发现服务如果失败重试3次
+                .setServiceDiscoverTimeout(20000)  // 发现服务超时20s
+                .build();
+
+        mClient.connect(connectMAC, options, new BleConnectResponse() {
             @Override
             public void onResponse(int code, BleGattProfile profile) {
                 if (code == REQUEST_SUCCESS) {
-                    Log.v("brad", "connected");
+                    Log.v("brad", "respone success");
+
+                    List<BleGattService> services = profile.getServices();
+                    for (BleGattService service : services){
+                        String uuid = service.getUUID().toString();
+                        //Log.v("brad", "service: " + uuid);
+
+                        List<BleGattCharacter> cs = service.getCharacters();
+                        for (BleGattCharacter c : cs){
+                            //Log.v("brad", "c: " + c);
+                        }
+
+                    }
+
                 }
             }
         });
@@ -91,7 +133,36 @@ public class MainActivity extends AppCompatActivity {
 
     private String connectMAC = null;
 
-    private class MySearchListener implements SearchResponse {      //負責搜尋回來之後做的事
+    private UUID serviceUUID = UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb");
+    private UUID characterUUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb");
+
+    public void notify(View view) {
+        Log.v("brad", connectMAC);
+        Log.v("brad", serviceUUID.toString());
+        Log.v("brad", characterUUID.toString());
+        mClient.refreshCache(connectMAC);
+        mClient.notify(connectMAC, serviceUUID, characterUUID, new BleNotifyResponse() {
+
+            @Override
+            public void onNotify(UUID service, UUID character, byte[] value) {
+                Log.v("brad", "notify");
+                for (int v : value){
+                    Log.v("brad", "value = " + v);
+                }
+            }
+
+            @Override
+            public void onResponse(int code) {
+                if (code == REQUEST_SUCCESS) {
+                    Log.v("brad", "success");
+                }else{
+                    Log.v("brad", "not success");
+                }
+            }
+        });
+    }
+
+    private class MySearchListener implements SearchResponse {
 
         @Override
         public void onSearchStarted() {
@@ -99,17 +170,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onDeviceFounded(SearchResult result) {          //找到的結果(原本result名字被設為device)
-            BluetoothDevice device = result.device;             //用結果挖出device才是裝置
+        public void onDeviceFounded(SearchResult result) {
+            BluetoothDevice device = result.device;
             String name = result.getName();
             String mac = result.getAddress();
-//            Log.v("brad",name + ":" + mac);
+            Log.v("brad", name + ":" + mac);
 
-            if(name.equals("BradAsus")){
-                Log.v("brad","I GOT IT!");
+            // BradAsus / Pixel 4 XL
+            if (name.equals("Pixel 4 XL")){
+                Log.v("brad", "i got it");
                 connectMAC = mac;
                 mClient.stopSearch();
+                mClient.registerConnectStatusListener(connectMAC, mBleConnectStatusListener);
             }
+
         }
 
         @Override
@@ -126,8 +200,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mClient.closeBluetooth();           //關藍芽
-        mClient.unregisterBluetoothStateListener(mBluetoothStateListener);
 
+        mClient.disconnect(connectMAC);
+        mClient.closeBluetooth();
+        mClient.unregisterBluetoothStateListener(mBluetoothStateListener);
+        mClient.unregisterConnectStatusListener(connectMAC, mBleConnectStatusListener);
     }
 }
